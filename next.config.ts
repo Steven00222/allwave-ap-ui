bd5db0b4f4108d6fc8accc84a3efbd4f8a12c692
Steven00222 <rehhdp@outlook.com>
2026-01-16 09:00:18 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    appDir: true, // ensures app/ folder is recognized
+  },
 };
 
 export default nextConfig;
